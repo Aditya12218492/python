22001860cbff80ce6b95a447254eba9161d4bd24
Aditya12218492 <119718325+Aditya12218492@users.noreply.github.com>
2022-12-23 02:19:52 +0530

    Add files via upload

---
+++ practice5.py
@@ -0,0 +1,6 @@
+a=int(input("enter the number "))
+b=int(input("enter the number "))
+c=int(input("enter the number "))
+d=int(input("enter the number "))
+e=int(input("enter the number "))
+print((a+b+c+d+e)/5)
